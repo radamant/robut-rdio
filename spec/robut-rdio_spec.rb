81cf3091c5768a4be3105ef6edeb1e8d9ff30f97
Jeremy Wadsack <jeremy.wadsack@gmail.com>
2011-11-12 12:16:53 -0800

    More spec cleanup

--- spec/robut-rdio_spec.rb
+++ spec/robut-rdio_spec.rb
@@ -2,15 +2,15 @@ require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 # Wherein we test legacy code, and hopefully refactor and remove this file
 describe "RobutRdio Super Integration Test" do
-  let(:plugin) do plugin = Robut::Plugin::Rdio.new(nil) 
+ 
+  let(:plugin) do 
+    plugin = Robut::Plugin::Rdio.new(nil) 
+
     def plugin.nick
       "dj"
     end
 
-
-    plugin.stub(:reply){|msg|
-      @reply = msg
-    }
+    plugin.stub(:reply){ |msg| @reply = msg }
 
     plugin
   end
@@ -21,7 +21,6 @@ describe "RobutRdio Super Integration Test" do
 
   describe "searching for tracks" do
 
-
     it 'should make an rdio search' do
       stub_search('neil young', ['harvest', 'after the gold rush'])
       say('@dj find neil young')
@@ -29,9 +28,9 @@ describe "RobutRdio Super Integration Test" do
     end
 
     def stub_search(mock_query, results)
-      plugin.stub(:search).with(['', mock_query]){results}
+      plugin.stub(:search).with(['', mock_query]) { results }
       results.each do |result|
-        plugin.stub(:format_result).with(result, anything()){"result: #{result}"}
+        plugin.stub(:format_result).with(result, anything()) { "result: #{result}" }
       end
     end
 
@@ -42,10 +41,10 @@ describe "RobutRdio Super Integration Test" do
 
     describe 'when there is a search result' do
       before do
-        plugin.stub(:result_at){|i| i.to_s}
-        plugin.stub(:queue){|result| @queued = result}
-        plugin.stub(:has_results?){true}
-        plugin.stub(:has_result?){true}
+        plugin.stub(:result_at) { |i| i.to_s }
+        plugin.stub(:queue) { |result| @queued = result }
+        plugin.stub(:has_results?) { true }
+        plugin.stub(:has_result?) { true }
       end
 
       it 'should queue the track at the given position with "play <number>"' do
@@ -60,7 +59,7 @@ describe "RobutRdio Super Integration Test" do
 
     describe 'when there are no search results' do
       before do
-        plugin.stub(:has_results?){false}
+        plugin.stub(:has_results?) { false }
       end
 
       it 'should say there are no results' do
@@ -71,8 +70,8 @@ describe "RobutRdio Super Integration Test" do
 
     describe 'when there are results but not at the requested index' do
       before do
-        plugin.stub(:has_results?){true}
-        plugin.stub(:has_result?).with(5){false}
+        plugin.stub(:has_results?) { true }
+        plugin.stub(:has_result?).with(5) { false }
       end
       it 'should say the result does not exist' do
         say '@dj play 5'
@@ -87,7 +86,7 @@ describe "RobutRdio Super Integration Test" do
 
   describe 'running commands' do
     before do
-      plugin.stub(:run_command){|command| @command = command}
+      plugin.stub(:run_command) { |command| @command = command }
     end
 
     %w{play unpause pause next restart back clear}.each do |command|
