5f18990ae33d2a4ba87f774cc7faa6c230b8b313
Franklin Webber <franklin.webber@gmail.com>
2011-12-05 01:28:03 -0800

    FIX: Performance for displaying of results
     * Should be amazingly faster now that it is not going back to the server
       each time for the names it already has on the object.

--- lib/robut-rdio.rb
+++ lib/robut-rdio.rb
@@ -203,12 +203,20 @@ class Robut::Plugin::Rdio
   def results
     @@results
   end
-
+  
+  
   private
   RESULT_DISPLAYER = {
-    ::Rdio::Album => lambda{|album| "#{album.artist.name} - #{album.name}"},
-    ::Rdio::Track => lambda{|track| "#{track.artist.name} - #{track.album.name} - #{track.name}"},
-    ::Rdio::Artist => lambda{|artist| "#{artist.name} - #{artist.tracks.sample.name}"}
+    ::Rdio::Album => lambda{|album| "#{album.artist_name} - #{album.name}"},
+    ::Rdio::Track => lambda{|track| "#{track.artist_name} - #{track.album_name} - #{track.name}"},
+    ::Rdio::Artist => lambda do |artist| 
+      tracks = artist.tracks(nil,0,1)
+      unless tracks.empty?
+        "#{artist.name} - #{tracks.first.album_name} - #{tracks.first.name}"
+      else
+        "#{artist.name}"
+      end
+    end
   }
 
   def run_command(command)
